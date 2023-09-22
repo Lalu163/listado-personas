5dc1d8f7b6089ee2c1fd8e63f541da0fca60e337
Laura Contreras <lauracontreras@MacBook-Pro-de-Laura.local>
2023-09-22 18:33:20 +0200

    binding entre componentes Output

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Persona } from '../persona.model';
 
 @Component({
   selector: 'app-formulario',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class FormularioComponent {
 
+  @Output() personaCreada = new EventEmitter<Persona>();
+
+  nombreInput:string = '';
+  apellidoInput:string = '';
+
+  agregarPersona(){
+    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+    this.personaCreada.emit(persona1);
+  }
 }
