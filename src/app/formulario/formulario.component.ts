5402d51f70374b0dfe919942cae5edea4354da18
Laura Contreras <lauracontreras@MacBook-Pro-de-Laura.local>
2023-09-26 20:13:44 +0200

    utilización nuevo servicio

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -25,6 +25,6 @@ export class FormularioComponent {
   agregarPersona(){
     let persona1 = new Persona(this.nombreInput.nativeElement.value, this.apellidoInput.nativeElement.value);
     this.loggingService.enviarMensajeAConsola("Enviamos persona con nombre: "+ persona1.nombre + " y apellido: "+ persona1.apellido)
-    this.personaCreada.emit(persona1);
+    //this.personaCreada.emit(persona1);  ya no se trabaja con un evento
   }
 }
